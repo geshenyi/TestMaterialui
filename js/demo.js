c3cda600aa79cef008a1f9762e2bd9414a93c92a
Chuck Ge <ssge@ebay.com>
2014-11-25 14:36:17 +0800

    first commit

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react'),
+    mui = require('material-ui'),
+    PaperButton = mui.PaperButton;
+
+var button = <PaperButton label="Demo"/>;
+
+React.render(button, document.body);
